767bb461d2094258a63c7de8db673d3627304ae7
ken <Tiensidien1234@gmail.com>
2026-03-05 16:02:46 +0700

    fix: delete eslint in next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
   async headers() {
     if (process.env.NODE_ENV === "development") {
       return [
